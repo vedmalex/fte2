d536f83a69936977439b0298e83cb27a02234236
vedanta-krit.das <vedmalex@gmail.com>
2017-06-30 17:04:53 +0300

    fix: demo file

--- demo/simple/test.js
+++ demo/simple/test.js
@@ -2,10 +2,10 @@ var Factory = require('../../dist/node').Factory;
 var fs = require('fs-extra');
 var s;
 var raw = new Factory({
-    root: 'raw',
-    debug: true,
+  root: 'raw',
+  debug: true,
 });
 
-s = raw.run({ content: 'TEXT', title: 'Some Title', head: { header: 'this is the header' }, greetings: 'Santa' }, 'Container2.nhtml');
+s = raw.run({ content: 'TEXT', title: 'Some Title', header: 'this is the header', greetings: 'Santa' }, 'Container2.nhtml');
 
 fs.writeFileSync('demo1.html', s);
